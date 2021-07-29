63b86b082ebf6fb3a77abf4b78aa3b3ba96b9851
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-29 14:17:29 +0530

    Added : Code to check if email samples provided are valid or not

--- src/main/java/com/registration/Registration.java
+++ src/main/java/com/registration/Registration.java
@@ -3,7 +3,8 @@ package com.registration;
 public class Registration {
     public static void main(String[] args) {
         UserRegistration userRegistration = new UserRegistration();
-        userRegistration.validateUserDetails();
+        //userRegistration.validateUserDetails();
+        userRegistration.email();
     }
 
 
--- src/main/java/com/registration/UserRegistration.java
+++ src/main/java/com/registration/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.registration;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -43,8 +44,43 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
+//Method to check whether the email samples are valid or not
+    public static void email() {
+        // Array list to store all sample emails.
+        ArrayList<String> emails = new ArrayList<String>();
+        emails.add("abc@yahoo.com");
+        emails.add("abc-100@yahoo.com");
+        emails.add("abc.100@yahoo.com");
+        emails.add("abc111@abc.com");
+        emails.add("abc-100@abc.net");
+        emails.add("abc.100@abc.com.au");
+        emails.add("abc@1.com");
+        emails.add("abc@gmail.com.com");
+        emails.add("abc+100@gmail.com");
+        emails.add("abc");
+        emails.add("abc@.com.my");
+        emails.add("abc123@gmail.a");
+        emails.add("abc123@.com");
+        emails.add("abc123@.com.com");
+        emails.add(".abc@abc.com");
+        emails.add("abc()*@gmail.com");
+        emails.add("abc@%*.com");
+        emails.add("abc..2002@gmail.com");
+        emails.add("abc.@gmail.com");
+        emails.add("abc@abc@gmail.com");
+        emails.add("abc@gmail.com.1a");
+        emails.add("abc@gmail.com.aa.au");
 
-        //Method to take the input from the user
+        String regexEmail = "^[0-9A-Za-z]+(([._+-])[0-9A-Za-z]+)*@[0-9A-Za-z]+.[a-z]{2,3}.([a-z]{2,3})*$";
+        Pattern pattern = Pattern.compile(regexEmail);
+
+        for (String email : emails) {
+            Matcher matcher = pattern.matcher(email);
+            System.out.println(email + " : " + matcher.matches() + "\n");
+        }
+    }
+
+    //Method to take the input from the user
         public void validateUserDetails () {
             Scanner sc = new Scanner(System.in);
             userDetails details = new userDetails();
