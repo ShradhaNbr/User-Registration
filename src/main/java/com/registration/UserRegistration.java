d4a8046e39efd9f3fba97913a4d43c96afcdc3ff
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-29 12:41:41 +0530

    Added : Code to check whether mobile number is valid or not

--- src/main/java/com/registration/UserRegistration.java
+++ src/main/java/com/registration/UserRegistration.java
@@ -14,6 +14,7 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
+
     //Method to check if email is valid or not
     public static boolean validateEmail(String email) {
         String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
@@ -21,32 +22,48 @@ public class UserRegistration {
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
     }
-//Method to take the input from the user
-    public void validateUserDetails() {
-        Scanner sc = new Scanner(System.in);
-        userDetails details = new userDetails();
-        UserRegistration registration = new UserRegistration();
-        System.out.println("Enter first name");
-        details.setFirstName(sc.next());
-        boolean first = registration.validateName(details.getFirstName());
-        if (first)
-            System.out.println("Valid");
-        else
-            System.out.println("Invalid");
-        System.out.println("Enter last name");
-        details.setLastName(sc.next());
-        boolean last = registration.validateName(details.getLastName());
-        if (last)
-            System.out.println("Valid");
-        else
-            System.out.println("Invalid");
-        System.out.println("Enter email id");
-        details.setEmail(sc.next());
-        boolean email = registration.validateEmail(details.getEmail());
-        if (email)
-            System.out.println("Valid");
-        else
-            System.out.println("Invalid");
-        sc.close();
+
+    //Method to check if phone number is valid or not
+    public static boolean validateMobileNumber(String phoneNumber) {
+        String regex = ("^(91)[0-9]{10}");
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
     }
+
+        //Method to take the input from the user
+        public void validateUserDetails () {
+            Scanner sc = new Scanner(System.in);
+            userDetails details = new userDetails();
+            UserRegistration registration = new UserRegistration();
+            System.out.println("Enter first name");
+            details.setFirstName(sc.next());
+            boolean first = registration.validateName(details.getFirstName());
+            if (first)
+                System.out.println("Valid");
+            else
+                System.out.println("Invalid");
+            System.out.println("Enter last name");
+            details.setLastName(sc.next());
+            boolean last = registration.validateName(details.getLastName());
+            if (last)
+                System.out.println("Valid");
+            else
+                System.out.println("Invalid");
+            System.out.println("Enter email id");
+            details.setEmail(sc.next());
+            boolean email = registration.validateEmail(details.getEmail());
+            if (email)
+                System.out.println("Valid");
+            else
+                System.out.println("Invalid");
+            System.out.println("Enter mobile number");
+            details.setMobileNumber(sc.next());
+            boolean mobileNumber = registration.validateMobileNumber(details.getMobileNumber());
+            if (mobileNumber)
+                System.out.println("Valid");
+            else
+                System.out.println("Invalid");
+            sc.close();
+        }
 }
--- src/main/java/com/registration/userDetails.java
+++ src/main/java/com/registration/userDetails.java
@@ -4,24 +4,42 @@ public class userDetails {
     String firstName;
     String lastName;
     String email;
+    String mobileNumber;
 
-    //using getters and setters
+    public userDetails() {
+
+    }
+    // Generating getters and setters.
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
 }
